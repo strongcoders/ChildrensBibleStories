d1a449ff1094bdd29e10499bf4e7c47ce09fe956
gdpags5 <gdpags5@gmail.com>
2014-09-03 00:47:35 +0800

    gpaguio[09/03/2014] - Modified the activity class and the layout for the audio settings.

--- src/com/childrensbiblestories/MainActivity.java
+++ src/com/childrensbiblestories/MainActivity.java
@@ -2,21 +2,26 @@ package com.childrensbiblestories;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends Activity
 {
     
     private Button btnSelectStory;
-    private Button btnSettings;
+    private Button btnStoryQuiz;
     private Button btnAbout;
+    private CheckBox cbPlayAudio;
     
     /** Called when the activity is first created. */
     @Override
@@ -39,12 +44,12 @@ public class MainActivity extends Activity
             }
         });
         
-        btnSettings = (Button) findViewById(R.id.btn_settings);
-        btnSettings.setOnClickListener(new OnClickListener() {
+        btnStoryQuiz = (Button) findViewById(R.id.btn_story_quiz);
+        btnStoryQuiz.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
+				Intent intent = new Intent(MainActivity.this, QuizDetailActivity.class);
+				startActivity(intent);
 			}
 		});
         
@@ -56,6 +61,30 @@ public class MainActivity extends Activity
                 startActivity(intent);
 			}
 		});
+        
+        // Initialize the shared preference object...
+        SharedPreferences sharedPref = getSharedPreferences("AudioPlay", Context.MODE_PRIVATE);
+        
+        cbPlayAudio = (CheckBox) findViewById(R.id.cb_play_audio);
+        // Get the status of the checkbox (Whether it is checked or not)
+        cbPlayAudio.setChecked(sharedPref.getBoolean("is_audio_play", false));
+        cbPlayAudio.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				SharedPreferences sharedPref = v.getContext().getSharedPreferences("AudioPlay", Context.MODE_PRIVATE);
+				Editor editor = sharedPref.edit();
+				
+				if (cbPlayAudio.isChecked()) {
+					editor.putBoolean("is_audio_play", true);
+				} else {
+					editor.putBoolean("is_audio_play", false);
+				}
+				
+				editor.commit();
+			}
+        	
+        });
     }
     
     @Override
